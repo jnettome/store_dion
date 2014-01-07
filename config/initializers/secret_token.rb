ad985e19223eebddf3dbf4f5b1f72b6413829b8a
João Netto <hi@joaonetto.me>
2014-01-07 11:33:56 -0200

    Initial commit. Spree was installed and translated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dionochner::Application.config.secret_key_base = '4571fa0adaaaacb9967e45ef869e7f191e49c4b6b81201610c421de8ca17c2481e07eb1ea611bbb94c1f2d073049f48127568393822b8dfff870e6cc16367608'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dionochner::Application.config.session_store :cookie_store, key: '_dionochner_session'
